fb9e4aeca2f055d04466c7d7962b580809e3438a
itsJRillo <jmorillolabour@gmail.com>
2022-10-21 19:14:45 +0200

    Sudoku Display

--- app/src/main/java/UF1/sudoku/MainActivity.java
+++ app/src/main/java/UF1/sudoku/MainActivity.java
@@ -2,32 +2,40 @@ package UF1.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TableLayout tabla = findViewById(R.id.sudoku);
+        Spinner sp = null;
         CharSequence[] numeros = {"1","2","3","4","5","6","7","8","9"};
+        TableLayout tabla = findViewById(R.id.sudoku);
 
         for(int i=0;i < 9;i++){
-            TableRow row = findViewById(R.id.row);
+            TableRow row = new TableRow(this);
             for(int j=0;j < 9;j++){
-                Spinner sp = new Spinner(this);
+                sp = new Spinner(this);
+
+
                 sp.setBackground(null);
                 sp.setPadding(5,5,5,5);
                 ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
                         android.R.layout.simple_spinner_item, numeros);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 sp.setAdapter(adapter);
+
                 row.addView(sp);
             }
             tabla.addView(row);
@@ -36,4 +44,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        int fila = (int) adapterView.getTag(R.id.fila);
+        int col = (int) adapterView.getTag(R.id.col);
+
+        Toast(String.valueOf(fila));
+        Toast(String.valueOf(col));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    public void Toast(CharSequence text){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
 }
---
+++ app/src/main/java/UF1/sudoku/SpinnerActivity.java
@@ -0,0 +1,14 @@
+package UF1.sudoku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpinnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spinner_activity);
+    }
+}
