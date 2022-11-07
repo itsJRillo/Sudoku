7703690ff7594eacd3744007257cd16df5127194
itsJRillo <jmorillolabour@gmail.com>
2022-11-07 15:37:30 +0100

    Styled Sudoku

--- app/src/main/java/UF1/sudoku/SudokuModel.java
+++ app/src/main/java/UF1/sudoku/SudokuModel.java
@@ -3,20 +3,34 @@ package UF1.sudoku;
 public class SudokuModel {
     private int[][] core = new int[9][9];
 
-    public int getVal(){
-        return 0;
+    public SudokuModel(int[][] core) {
+        this.core = core;
     }
 
-    public void setVal(){
-
+    public int getVal(int fila, int col){
+        return core[fila][col];
     }
 
-    public void comprovarFila(){
-
+    public void setVal(int fila, int col, int valor){
+        core[fila][col] = valor;
     }
 
-    public void comprovarCol(){
+    public int comprovarFila(int fila, int valor){
+        for(int i=0;i < 9;i++){
+            if(core[fila][i] == valor){
+                return 0;
+            }
+        }
+        return -1;
+    }
 
+    public int comprovarCol(int col, int valor){
+        for(int i=0;i < 9;i++){
+            if(core[i][col] == valor){
+                return 0;
+            }
+        }
+        return -1;
     }
 
     public void comprovarQuad(){
